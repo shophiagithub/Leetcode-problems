c2d205c72f276a0e078be2947c3f3a343a251ab7
shophiagithub <114874837+shophiagithub@users.noreply.github.com>
2024-05-12 23:32:41 +0530

    Time: 11 ms (85.47%) | Memory: 11.7 MB (10.20%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a=0
+        b=1
+        for i in range(n):
+            a,b=b,a+b
+        return a
+        
+        
+        
+        
+        
