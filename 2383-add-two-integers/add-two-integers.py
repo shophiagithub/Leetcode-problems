a14576dcbf28d7f23a61ab7063151a08340f91f2
shophiagithub <114874837+shophiagithub@users.noreply.github.com>
2024-05-12 23:34:29 +0530

    Time: 11 ms (76.55%) | Memory: 11.6 MB (72.41%) - LeetSync

---
+++ 2383-add-two-integers/add-two-integers.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        result=num1+num2
+        return result
+        
