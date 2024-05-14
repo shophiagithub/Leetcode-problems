0d8c294fa7b638e6c8859b30fb7a2e9c82751d19
shophiagithub <114874837+shophiagithub@users.noreply.github.com>
2024-05-14 22:34:26 +0530

    Time: 23 ms (43.61%) | Memory: 11.7 MB (87.66%) - LeetSync

---
+++ 415-add-strings/add-strings.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        n1=int(num1)
+        n2=int(num2)
+        result=n1+n2
+        new_str=str(result)
+        return new_str
+        
