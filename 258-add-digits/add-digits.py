790ee45eb388b62777f50b5ee118089189528be3
shophiagithub <114874837+shophiagithub@users.noreply.github.com>
2024-05-15 00:19:35 +0530

    Time: 14 ms (74.84%) | Memory: 11.5 MB (75.82%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -6,7 +6,7 @@ class Solution(object):
         """
         if num==0:
             return 0
-        while(num>=10):
+        while(num>9):
                 digit=num%10+num//10
                 num=digit
         return num
