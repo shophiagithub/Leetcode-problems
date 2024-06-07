020cc0c5ac387e34f8466101544f0c34fa54c7c6
shophiagithub <114874837+shophiagithub@users.noreply.github.com>
2024-06-07 20:44:58 +0530

    Time: 14 ms (79.39%) | Memory: 11.6 MB (74.63%) - LeetSync

---
+++ 67-add-binary/add-binary.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        s=[]
+        carry=0
+        i=len(a)-1
+        j=len(b)-1
+        while(i>=0 or j>=0 or carry):
+            if i>=0:
+                carry=carry+int(a[i])
+                i-=1
+            if j>=0:
+                carry=carry+int(b[j])
+                j-=1
+            s.append(str(carry%2))
+            carry//=2
+        return ''.join(reversed(s))
+        
+        
+        
+        
