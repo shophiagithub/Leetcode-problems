09b967b682830d8f36046b357d1dd591ad9d1a24
Shophia <114874837+shophiagithub@users.noreply.github.com>
2024-07-28 17:18:31 +0530

    Time: 25 ms (27.38%) | Memory: 11.8 MB (5.71%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,11 @@
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        sqrt_x=math.sqrt(x)
+        sqrt_x=int(sqrt_x)
+
+        return sqrt_x
