dba882966bad4a16e627e12fff66cea6dc0a0f51
Shophia <114874837+shophiagithub@users.noreply.github.com>
2024-07-28 19:44:51 +0530

    Time: 15 ms (82.46%) | Memory: 11.6 MB (77.62%) - LeetSync

--- 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
+++ 3412-permutation-difference-between-two-strings/permutation-difference-between-two-strings.py
@@ -5,7 +5,14 @@ class Solution(object):
         :type t: str
         :rtype: int
         """
+        s1={}
+        t1={}
+        #key-character,value-index
+        n=len(s)
+        for i in range(n):
+            s1[s[i]]=i
+            t1[t[i]]=i
         sum=0
-        for i in s:
-            sum=sum+abs(s.index(i)-t.index(i))
+        for char,index in s1.items():
+            sum=sum+abs(index-t1[char])
         return sum
