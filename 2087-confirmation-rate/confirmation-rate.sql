e97139477cfacd19ccf8b8dea906501ce0e76331
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-09-06 19:21:20 +0530

    Time: 547 ms (99.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,8 @@
+SELECT s.user_id,
+IFNULL(ROUND(SUM(CASE WHEN c.action='confirmed' THEN 1 ELSE 0 END)/
+COUNT(c.action),2),0) AS confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c
+ON s.user_id=c.user_id
+GROUP BY s.user_id;
+
