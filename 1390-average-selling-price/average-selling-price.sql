98b71346dd2bc480909b6d72e02f33c9c38d6fa6
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-09-07 22:55:17 +0530

    Time: 711 ms (93.17%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1390-average-selling-price/average-selling-price.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT p.product_id,
+-- (CASE
+-- WHEN u.units IS NULL AND u.product_id IS NULL THEN 0
+IFNULL(ROUND(SUM(u.units*p.price)/SUM(u.units),2),0)  AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u
+ON p.product_id=u.product_id AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;
