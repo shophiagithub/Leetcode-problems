e4c8e3e1eaedd30530379590d4abf0e12168a393
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-09-11 22:36:25 +0530

    Time: 574 ms (51.59%) | Memory: 0B (100.00%) - LeetSync

--- 1182-game-play-analysis-iv/game-play-analysis-iv.sql
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -4,7 +4,7 @@ WITH cte AS(
     FROM Activity 
     GROUP BY player_id
 )
-SELECT ROUND(COUNT(DISTINCT(a.player_id))/
+SELECT ROUND(COUNT(a.player_id)/
 (SELECT COUNT(DISTINCT(player_id))
 FROM Activity),2) AS fraction
 FROM Activity a
