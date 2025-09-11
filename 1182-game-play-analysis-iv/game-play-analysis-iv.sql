352049c30a7d68dd945b9ee27e1b6d13127b2937
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-09-11 22:35:44 +0530

    Time: 1163 ms (8.89%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+WITH cte AS(
+    SELECT player_id,MIN(event_date) AS first_date
+    FROM Activity 
+    GROUP BY player_id
+)
+SELECT ROUND(COUNT(DISTINCT(a.player_id))/
+(SELECT COUNT(DISTINCT(player_id))
+FROM Activity),2) AS fraction
+FROM Activity a
+JOIN cte c ON a.player_id=c.player_id
+AND a.event_date=DATE_ADD(c.first_date,INTERVAL 1 DAY);
