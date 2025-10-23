d6a0042c48037e6292f22006baf4dd98f77be395
Shophia <114874837+shophiagithub@users.noreply.github.com>
2025-10-23 10:04:32 +0530

    Time: 273 ms (41.44%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,7 +1,12 @@
 # Write your MySQL query statement below
-    SELECT(
-        SELECT DISTINCT salary 
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET 1
-    ) AS SecondHighestSalary;
+-- Using windows function and MAX
+WITH cte AS(SELECT 
+salary,
+DENSE_RANK() OVER(
+    ORDER BY salary DESC
+) AS rank_num
+FROM Employee)
+
+SELECT 
+MAX(CASE WHEN rank_num=2 THEN salary END) AS SecondHighestSalary
+FROM cte;
